3604ebaa75497da4cc9069db30097ee01289eb8a
LuxitoKun <44988724+LuxitoKun@users.noreply.github.com>
2019-01-21 00:43:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log('I am ready!');
 });
-client.user.setActivity("Minecraft", {type: "PLAYING"});
                                
 client.on('message', message => {
     if (message.content === 'ping') {
