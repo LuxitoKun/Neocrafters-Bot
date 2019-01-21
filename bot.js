1c98dd705292a62d3976640a5e63a76122607ac7
LuxitoKun <44988724+LuxitoKun@users.noreply.github.com>
2019-01-21 00:58:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.setActivity('YT!', { type: 'WATCHING' });
 client.on('ready', () => {
     console.log('I am ready!');
 });
                                
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === 'ip') {
+    	message.reply('mc.neocrafters.es');
   	}
 });
 
