5e3a0941043763080489812d8d89f244743ae54e
LuxitoKun <44988724+LuxitoKun@users.noreply.github.com>
2019-01-21 00:34:53 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,69 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const settings = require('./config.json');
-var statustring = "No signal";
 
-var request = require('request');
-var mcCommand = '/neo'; // Command for triggering
-var mcIP = settings.ip; // Your MC server IP
-var mcPort = settings.port; // Your MC server port
-
-var url = 'http://mcapi.us/server/status?ip=' + mcIP + '&port=' + mcPort;
-
-
-function update() {
-  seconds = seconds + 1;
-  secondsString = seconds.toString();
-  client.user.setActivity(secondsString, { type: 'Playing' })
-  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
-  .catch(console.error);
-  request(url, function(err, response, body) {
-      if(err) {
-          console.log(err);
-          return message.reply('Error getting Minecraft server status...');
-      }
-      body = JSON.parse(body);
-      var status = 'Server offline';
-      console.log(body.motd);
-      if(body.online) {
-          if((body.motd=="&cWe are under maintenance.")||(body.players.now>=body.players.max)){
-            client.user.setStatus('idle')
-            //.then(console.log)
-            .catch(console.error);
-          }else{
-            client.user.setStatus('online')
-            //.then(console.log)
-            .catch(console.error);
-          }
-            if(body.players.now) {
-                status = ' ' + body.players.now + '  of  ' + body.players.max;
-              } else {
-                status = ' 0  of  ' + body.players.max;
-        }
-      } else {
-        client.user.setStatus('dnd')
-        //.then(console.log)
-        .catch(console.error);
-
-      }
-      client.user.setActivity(status, { type: 'PLAYING' })
-      .then(presence => console.log(status))
-      .catch(console.error);
-  });
-
-}
-client.on("ready", () => {
-  console.log("I am ready!");
-  client.setInterval(update,30000);
+client.on('ready', () => {
+    client.user.setActivity("mc.neocrafters.es")
+    console.log('I am ready!');
 });
 
-/*client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-    update();
-  }
-}
-);*/
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
