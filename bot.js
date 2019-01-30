19b5c467f29f025275dc20adc8cce0afe4e356d7
LuxitoKun <44988724+LuxitoKun@users.noreply.github.com>
2019-01-29 21:51:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ip') {
     	message.reply('mc.neocrafters.es');
+        client.user.setPresence({ activity: { name: mc.neocrafters.es }, status 'online' }); 
   	}
 });
 
